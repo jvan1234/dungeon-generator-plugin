260d1c2fe33e77b15b2c95c348d6c5a29c6edfa6
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-01 16:41:10 -0400

    Just testing things for the plugin

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ function Generate_Dungeon (length: number, width: number) {
     tiles.setCurrentTilemap(tilemap`level1`)
     for (let xIndex = 0; xIndex <= length; xIndex++) {
         for (let yIndex = 0; yIndex <= width; yIndex++) {
+            tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), sprites.dungeon.darkGroundCenter)
             if (xIndex == 0 || xIndex == length || (yIndex == 0 || yIndex == width)) {
                 tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), sprites.dungeon.floorLight0)
                 tiles.setWallAt(tiles.getTileLocation(xIndex, yIndex), true)
