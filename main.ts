4c4e0e94d6c01c1c026fb205ba2a7ea1fabd3d05
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-01 16:17:40 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,1 @@
-function Generate_Dungeon (length: number, width: number) {
-    tiles.setCurrentTilemap(tilemap`level1`)
-    for (let xIndex = 0; xIndex <= length; xIndex++) {
-        for (let yIndex = 0; yIndex <= width; yIndex++) {
-            tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), sprites.dungeon.floorLight0)
-        }
-    }
-}
+console.log("HELP ME")
