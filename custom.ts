cb6a3ad9337297cb4e3084a4636d0fb34e6ab8da
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-03 17:18:18 -0400

    Plugin can now generate mazes!

--- custom.ts
+++ custom.ts
@@ -19,8 +19,8 @@ namespace dungeons {
     //% block
     
     export function Generate_Dungeon(length: number, width: number, wall_chance: number) {
-        let Digger: Sprite = null
         let currentPosition: tiles.Location = null
+        let Digger: Sprite = null
         tiles.setCurrentTilemap(tilemap`level1`)
         for (let xIndex = 0; xIndex <= length - 1; xIndex++) {
             for (let yIndex = 0; yIndex <= width - 1; yIndex++) {
@@ -45,8 +45,12 @@ namespace dungeons {
             console.log("" + currentPosition.column + ", " + currentPosition.row)
             if (Math.percentChance(50) && currentPosition.column < length - 2) {
                 tiles.placeOnTile(Digger, tiles.getTileLocation(currentPosition.column + 1, currentPosition.row))
-            } else if (Math.percentChance(100) && currentPosition.row < width - 2) {
+            } else if (Math.percentChance(50) && currentPosition.row < width - 2) {
                 tiles.placeOnTile(Digger, tiles.getTileLocation(currentPosition.column, currentPosition.row + 1))
+            } else if (Math.percentChance(50) && currentPosition.column > 1) {
+                tiles.placeOnTile(Digger, tiles.getTileLocation(currentPosition.column - 1, currentPosition.row))
+            } else if (currentPosition.row > 1) {
+                tiles.placeOnTile(Digger, tiles.getTileLocation(currentPosition.column, currentPosition.row - 1))
             }
             if (!(Digger.tileKindAt(TileDirection.Center, sprites.dungeon.stairLarge) || Digger.tileKindAt(TileDirection.Center, sprites.dungeon.collectibleInsignia))) {
                 tiles.setTileAt(Digger.tilemapLocation(), sprites.dungeon.darkGroundCenter)
@@ -55,4 +59,5 @@ namespace dungeons {
         }
         sprites.destroy(Digger)
     }
+    
 }
